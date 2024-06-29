791d4082f640a866c9dacd85b94715f0898f6a8c
apanyaclay <apanyaclay1@gmail.com>
2024-06-30 03:47:18 +0700

    add: create new assets

---
+++ public/ckeditor/config.js
@@ -0,0 +1,15 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	config.language = 'en';
+	// config.uiColor = '#F7B42C';
+    config.height = 300;
+	config.toolbarCanCollapse = true;
+    // Changes the theme to "ir-black".
+    config.codeSnippet_theme = 'ir-black';
+
+};
